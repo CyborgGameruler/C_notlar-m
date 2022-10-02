7a7df3afe9f46e3054585b600ca96534857f5684
unknown <894297@gmail.com>
2022-10-02 10:36:12 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# C_notlar-m
---
+++ ders_notlarım_1.c
@@ -0,0 +1,60 @@
+
+
+//tek satırlık yorum satırı
+
+/*
+yorum bölgesi
+
+*/
+
+/*Temel Değişkeler
+
+char,int,float,double,short,long
+
+char = karakter
+int = tamsayı
+short(short int) = küçük tamsayı
+long(lont int) = büyük tamsayı
+float = kesirli tam sayı
+double = kesirli tam sayı(daha kapsamlı)
+*/
+/*
+Değişken tanımlama
+Veritipi değişkenadi ;
+*/
+/*Format belirleyici
+%d ----->int değerler için printf de format belirleyici olarak kullanılır
+
+int x = 5;
+printf("d",x); ekrana 5 basar
+
+%f ---->float ve double için format belirleyici olarak kullanılır.
+
+float f=2.1
+double d =3.11
+printf("%f %f",f,d); ----> 2.1 ve 3.11 basar
+
+%c -----> karakterler için kullanılır
+
+char c = 'c';
+
+printf("%c",c);----->ekrana c basar
+printf("d",c);------>ekrana ASCII tablosundaki c değerini basar
+
+%s ----> karakter dizeleri için kullanılır
+
+printf("%s","adam"); ---->ekran adam basar :D
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
